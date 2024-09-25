b49e1b72cd873df959cfe0e6d8816b488211fd30
Terrence Nolan <terrencenolan@proton.me>
2024-09-25 18:07:56 -0400

    feat(layout): add fonts and colorscheme to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,35 @@
+
+import { useEffect } from "react";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </ThemeProvider>
   );
 }

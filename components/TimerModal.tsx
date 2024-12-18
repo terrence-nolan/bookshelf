49e2059ad7995ad81dc2d7e50d3647f7be499bb5
Terrence Nolan <terrencenolan@proton.me>
2024-12-17 20:01:20 -0500

    feat: create timer modal with ability to adjust time goal and counter

--- components/TimerModal.tsx
+++ components/TimerModal.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, FlatList, TouchableOpacity, SafeAreaView, Modal } from 'react-native';
+import { View, Text, TouchableOpacity, Modal } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { ArrowCounterClockwise, Check, Pause, Pencil, Play } from 'phosphor-react-native';
-import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
-import IconButton from './IconButton';
+import BottomSheet, { BottomSheetView, BottomSheetBackdrop } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface TimerProps {
   isVisible: boolean;
@@ -39,13 +39,6 @@ export function TimerModal({ isVisible, setIsVisible }: TimerProps) {
       clearInterval(timer);
     };
   }, [isRunning]);
-
-  // const formatTime = (seconds: number) => {
-  //   const hrs = Math.floor(seconds / 3600);
-  //   const mins = Math.floor((seconds % 3600) / 60);
-  //   const secs = seconds % 60;
-  //   return `${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
-  // };
 
   const formatTime = (seconds: number) => {
     const hrs = Math.floor(seconds / 3600);
@@ -91,88 +84,112 @@ export function TimerModal({ isVisible, setIsVisible }: TimerProps) {
     if (bottomSheetRef.current) {
       bottomSheetRef.current.expand();
     }
-  }
+  };
 
   const formatGoal = (minutes: number) => {
     const hrs = Math.floor(minutes / 60);
     const mins = minutes % 60;
 
-    if (hrs > 0) {
+    if (hrs > 0 && mins !== 0) {
       return `${hrs} hour and${mins > 0 ? ` ${mins} minute` : ''}`;
+    } else if (hrs > 0 && mins == 0) {
+      return `${hrs} hour`;
     }
-    return `${mins} ${mins === 1 ? 'minute' : 'minutes'}`;
+    return `${mins} minute`;
   };
 
   return (
     <Modal
+      key={isVisible ? 'visible' : 'hidden'}
       visible={isVisible}
       animationType='slide'
+      onRequestClose={() => setIsVisible()}
+      onDismiss={() => bottomSheetRef.current?.close()}
     >
-      <SafeAreaView />
-      <View className="flex-1">
-        <View className="flex-row w-full justify-between pb-2 border-b">
-          <TouchableOpacity onPress={() => setIsVisible()}>
-            <Text className="text-xl pl-5">Close</Text>
-          </TouchableOpacity>
-        </View>
-        <View className='flex-1 bg-red-500 flex-col justify-center items-center p-5 mt-10'>
-          <Text className="text-7xl font-bold">{formatTime(elapsedTime)}</Text>
-          <Text className="text-lg">of my {formatGoal(dailyGoal)} goal</Text>
-          <View className="flex-row gap-10 my-10">
-            <TouchableOpacity
-              onPress={handleStartStop}
-              className={`p-4 rounded-full ${isRunning ? 'bg-stone-950' : 'bg-stone-950'}`}
-            >
-              {isRunning? <Pause size={28} color='white' /> : <Play size={28} color='white' />} 
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={handleLog}
-              className={`p-4 rounded-full ${!isRunning && elapsedTime > 0 ? 'bg-stone-950' : 'bg-stone-300'}`}
-              disabled={isRunning || elapsedTime === 0}
-            >
-              <Check size={28} color='white' />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={handleClear}
-              className={`p-4 rounded-full ${!isRunning && elapsedTime > 0 ? 'bg-stone-950' : 'bg-stone-300'}`}
-              disabled={isRunning || elapsedTime === 0}
-            >
-              <ArrowCounterClockwise size={28} color='white' />
+      {/* might need to change flex-1 to style={{ flex: 1 }} */}
+      <SafeAreaView className="flex-1">
+        <View className="flex-1">
+          {/* Header */}
+          <View className="flex-row w-full justify-between pb-2 border-b">
+            <TouchableOpacity onPress={() => setIsVisible()}>
+              <Text className="text-xl pl-5">Close</Text>
             </TouchableOpacity>
           </View>
-          <TouchableOpacity
-            className='bg-stone-950 py-4 px-6 rounded-full'
-            onPress={openBottomSheet}
-          >
-            <Text className='text-stone-50 font-medium text-lg'>Adjust My Goal</Text>
-          </TouchableOpacity>
-        </View>
-        <GestureHandlerRootView className='bg-purple-500'>
-          <BottomSheet
-            ref={bottomSheetRef}
-            index={-1}
-            snapPoints={['75%']}
-            enablePanDownToClose={false}
-            enableHandlePanningGesture={false}
-            enableContentPanningGesture={false}
-            handleComponent={null}
-          >
-            <BottomSheetView className="bg-white p-5 flex-col justify-center items-center">
-              <Text className="text-lg mb-3">Set Daily Goal</Text>
-              <Picker
-                selectedValue={dailyGoal.toString()}
-                onValueChange={(value) => setDailyGoal(Number(value))}
-                style={{ height: 50, width: '100%' }}
-              >
-                {Array.from({ length: 1440 }, (_, i) => i + 1).map((value) => (
-                  <Picker.Item label={`${value.toString()} ${value === 1 ? 'minute' : 'minutes'}`} value={value.toString()} key={value} />
-                ))}
 
-              </Picker>
-            </BottomSheetView>
-          </BottomSheet>
-      </GestureHandlerRootView>
-      </View>
+          {/* Main Content */}
+          <View className="flex-1 flex-col justify-center items-center px-5">
+            <View className='items-center'>
+              <Text className="text-8xl font-bold mb-5">{formatTime(elapsedTime)}</Text>
+              <Text className="text-lg mb-10">of my {formatGoal(dailyGoal)} goal</Text>
+
+              <View className="flex-row gap-10">
+                <TouchableOpacity
+                  onPress={handleStartStop}
+                  className={`p-4 rounded-full ${isRunning ? 'bg-stone-950' : 'bg-stone-950'}`}
+                >
+                  {isRunning ? <Pause size={28} color='white' /> : <Play size={28} color='white' />}
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  onPress={handleLog}
+                  className={`p-4 rounded-full ${!isRunning && elapsedTime > 0 ? 'bg-stone-950' : 'bg-stone-300'}`}
+                  disabled={isRunning || elapsedTime === 0}
+                >
+                  <Check size={28} color='white' />
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  onPress={handleClear}
+                  className={`p-4 rounded-full ${!isRunning && elapsedTime > 0 ? 'bg-stone-950' : 'bg-stone-300'}`}
+                  disabled={isRunning || elapsedTime === 0}
+                >
+                  <ArrowCounterClockwise size={28} color='white' />
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            {/* Adjust Goal Button */}
+            <View className='absolute bottom-5 left-0 right-0 items-center'>
+              <TouchableOpacity
+                className='bg-stone-950 py-4 px-6 rounded-full'
+                onPress={openBottomSheet}
+              >
+                <Text className='text-stone-50 font-medium text-lg'>Adjust My Goal</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+
+        {/* Bottom Sheet */}
+        <BottomSheet
+          ref={bottomSheetRef}
+          index={-1}
+          snapPoints={['33%']}
+          enablePanDownToClose={true}
+          backdropComponent={(props) => (
+            <BottomSheetBackdrop {...props} opacity={0.5} disappearsOnIndex={-1} appearsOnIndex={0} />
+          )}
+        >
+          <BottomSheetView
+            className="bg-white p-5 flex-col justify-center items-center"
+          >
+            <Text className="text-xl font-medium">Set Daily Goal</Text>
+            <Picker
+              selectedValue={dailyGoal.toString()}
+              onValueChange={(value) => setDailyGoal(Number(value))}
+              style={{ height: 50, width: '100%' }}
+            >
+              {Array.from({ length: 1440 }, (_, i) => i + 1).map((value) => (
+                <Picker.Item
+                  label={`${value.toString()} ${value === 1 ? 'minute' : 'minutes'}`}
+                  value={value.toString()}
+                  key={value}
+                />
+              ))}
+            </Picker>
+          </BottomSheetView>
+        </BottomSheet>
+      </SafeAreaView>
     </Modal>
   );
 }

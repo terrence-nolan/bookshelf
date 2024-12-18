8554563e7b4ebabf95705a6d1d245b8888f4dc0e
Terrence Nolan <terrencenolan@proton.me>
2024-12-18 00:45:37 -0500

    feat(components): add status bar and daily status text

--- components/TimerModal.tsx
+++ components/TimerModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { View, Text, TouchableOpacity, Modal } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { ArrowCounterClockwise, Check, Pause, Pencil, Play } from 'phosphor-react-native';
@@ -19,24 +19,29 @@ interface TimerLog {
 }
 
 export function TimerModal({ isVisible, setIsVisible }: TimerProps) {
+  const { top, bottom } = useSafeAreaInsets();
   const [isRunning, setIsRunning] = useState<boolean>(false);
   const [elapsedTime, setElapsedTime] = useState<number>(0);
   const [startTime, setStartTime] = useState<Date | null>(null);
   const [totalLoggedTime, setTotalLoggedTime] = useState<number>(0);
-  const [dailyGoal, setDailyGoal] = useState<number>(0);
+  const [totalLoggedMinutes, setTotalLoggedMinutes] = useState<number>(0);
+  const [dailyGoal, setDailyGoal] = useState<number>(1);
   const bottomSheetRef = useRef<BottomSheet>(null);
 
-  useEffect(() => {
-    let timer: NodeJS.Timeout;
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
 
+  useEffect(() => {
     if (isRunning) {
-      timer = setInterval(() => {
+      timerRef.current = setInterval(() => {
         setElapsedTime((prev) => prev + 1);
       }, 1000);
+    } else if (timerRef.current) {
+      clearInterval(timerRef.current);
+      timerRef.current = null;
     }
 
     return () => {
-      clearInterval(timer);
+      if (timerRef.current) clearInterval(timerRef.current);
     };
   }, [isRunning]);
 
@@ -67,6 +72,7 @@ export function TimerModal({ isVisible, setIsVisible }: TimerProps) {
   const handleLog = () => {
     if (!isRunning && elapsedTime > 0) {
       setTotalLoggedTime((prev) => prev + elapsedTime);
+      setTotalLoggedMinutes((prev) => (prev + elapsedTime)/60)
       setElapsedTime(0);
       setStartTime(null);
     }
@@ -86,6 +92,11 @@ export function TimerModal({ isVisible, setIsVisible }: TimerProps) {
     }
   };
 
+  const pickerOptions = useMemo(() => {
+    return Array.from({ length: 1440 }, (_, i) => i + 1);
+  }, []);
+  
+
   const formatGoal = (minutes: number) => {
     const hrs = Math.floor(minutes / 60);
     const mins = minutes % 60;
@@ -100,32 +111,41 @@ export function TimerModal({ isVisible, setIsVisible }: TimerProps) {
 
   return (
     <Modal
-      key={isVisible ? 'visible' : 'hidden'}
       visible={isVisible}
       animationType='slide'
       onRequestClose={() => setIsVisible()}
       onDismiss={() => bottomSheetRef.current?.close()}
     >
-      {/* might need to change flex-1 to style={{ flex: 1 }} */}
-      <SafeAreaView className="flex-1">
+      <View style={{ paddingTop: top, paddingBottom: bottom }} className="flex-1">
         <View className="flex-1">
           {/* Header */}
-          <View className="flex-row w-full justify-between pb-2 border-b">
+          <View className="flex-row w-full pb-2 border-b">
             <TouchableOpacity onPress={() => setIsVisible()}>
               <Text className="text-xl pl-5">Close</Text>
             </TouchableOpacity>
           </View>
 
+          <View className='flex flex-col gap-4 items-center mt-10'>
+            {totalLoggedMinutes >= dailyGoal ? (
+              <Text className='text-md'>Congrats! You have completed your {dailyGoal} minute goal.</Text>
+            ) : (
+              <Text className='text-md'>You have read for {Math.floor(totalLoggedMinutes)} {Math.floor(totalLoggedMinutes) === 1 ? 'minute' : 'minutes'} of your {dailyGoal} minute goal.</Text>
+            )}
+            <View className='bg-stone-300 h-2 w-10/12 rounded-full'>
+              <View style={{ width: (totalLoggedTime*(10/12)/(dailyGoal)), minWidth: 7 }} className='bg-green-600 h-2 rounded-full' />
+            </View>
+          </View>
+
           {/* Main Content */}
-          <View className="flex-1 flex-col justify-center items-center px-5">
+          <View style={{ marginTop: -80 }} className="flex-1 flex-col justify-center items-center px-5">
             <View className='items-center'>
-              <Text className="text-8xl font-bold mb-5">{formatTime(elapsedTime)}</Text>
-              <Text className="text-lg mb-10">of my {formatGoal(dailyGoal)} goal</Text>
+              <Text className="text-8xl font-bold">{formatTime(elapsedTime)}</Text>
+              <Text className='text-lg mb-10'>{elapsedTime === 0 && !isRunning ? `Start a new session.` : `elapsed in this session.`}</Text>
 
               <View className="flex-row gap-10">
                 <TouchableOpacity
                   onPress={handleStartStop}
-                  className={`p-4 rounded-full ${isRunning ? 'bg-stone-950' : 'bg-stone-950'}`}
+                  className={`p-4 rounded-full bg-stone-950`}
                 >
                   {isRunning ? <Pause size={28} color='white' /> : <Play size={28} color='white' />}
                 </TouchableOpacity>
@@ -179,9 +199,9 @@ export function TimerModal({ isVisible, setIsVisible }: TimerProps) {
               onValueChange={(value) => setDailyGoal(Number(value))}
               style={{ height: 50, width: '100%' }}
             >
-              {Array.from({ length: 1440 }, (_, i) => i + 1).map((value) => (
+              {pickerOptions.map((value) => (
                 <Picker.Item
-                  label={`${value.toString()} ${value === 1 ? 'minute' : 'minutes'}`}
+                  label={`${value.toString()} min/day`}
                   value={value.toString()}
                   key={value}
                 />
@@ -189,7 +209,7 @@ export function TimerModal({ isVisible, setIsVisible }: TimerProps) {
             </Picker>
           </BottomSheetView>
         </BottomSheet>
-      </SafeAreaView>
+      </View>
     </Modal>
   );
 }

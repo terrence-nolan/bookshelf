f2e91cf79cf39d26653243d239ccb7c50be30f41
Terrence Nolan <terrencenolan@proton.me>
2025-01-16 23:48:39 -0500

    feat: handle selection of radio bar

--- components/RadioBar.tsx
+++ components/RadioBar.tsx
@@ -4,17 +4,23 @@ import { View, Text, TouchableOpacity } from "react-native";
 interface RadioBarProps {
   selectionOptions: string[];
   defaultSelection: string;
+  onSelect: (selection: string) => void;
 }
 
-export function RadioBar({ selectionOptions, defaultSelection }: RadioBarProps) {
+export function RadioBar({ selectionOptions, defaultSelection, onSelect }: RadioBarProps) {
   const [selected, setSelected] = useState(defaultSelection);
+
+  const handleSelect = (option: string) => {
+    setSelected(option);
+    onSelect(option);
+  }
 
   return (
     <View className="flex flex-row gap-2 items-center">
       {selectionOptions.map((option) => (
         <TouchableOpacity
           key={option}
-          onPress={() => setSelected(option)}
+          onPress={() => handleSelect(option)}
           className={`px-3 py-1 rounded-full ${
             selected === option ? "bg-stone-950" : "bg-stone-200"
           }`}
